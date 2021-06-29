2aae5a51206770558a8e93f3f5820eeaccb8cc68
kali <kalirk347@gmail.com>
2021-06-29 16:41:43 +0530

    dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
+//The Elite 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child:  Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -14,10 +15,46 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  var left=2;
+  var right=4;
+  void get()
+  {
+    setState(() {
+      right=Random().nextInt(6)+1;
+      left=Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                get();
+              },
+              child: Image.asset('images/dice$left.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed:(){
+                get();
+              },
+              child: Image.asset('images/dice$right.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
